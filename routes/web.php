38d9eb0969c3390de5ff7d43b94e0116d679217f
liu <461043365@qq.com>
2019-04-17 11:01:49 +0000

    改

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,5 @@ Route::get('/index/send','TestController@send');
 
 //微信支付
 Route::get('/weixin/pay/test','Weixin\WxPayController@test');
-Route::get('/weixin/pay/notify','Weixin\WxPayController@notify');       //微信支付回调地址
+Route::post('/weixin/pay/notify','Weixin\WxPayController@notify');       //微信支付回调地址
 
